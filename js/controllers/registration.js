cea515dca6ac5f596c01c3256eafe95344510af3
Utsav Agarwal <uagarwal744@gmail.com>
2016-06-15 13:17:49 +0530

    Insertion working

--- js/controllers/registration.js
+++ js/controllers/registration.js
@@ -1,19 +1,42 @@
 myApp.controller('RegistrationController', function($scope) {
   
-   $scope.data = {
-      group1 : 'Car'
-    };
+    $scope.dats = {
+   //    group1 : 'Car',
+   //    start_lat: null,
+   //    start_lng:null,
+   //    start_time:null,
+   //    start_location:null,
+   //    end_lat: null,
+   //    end_lng:null,
+   //    end_time:null,
+   //    end_location:null,
+   //    purpose:null,
+   //    project_id:null,
+   //    mode:null,
+   //    active:null,
+      availableModes:[
+        {mode:"car"},
+        {mode:"bike"},
+        {mode:"metro"},
+        {mode:"bus"}
+      ]
+     };
+     $scope.repeatSelect;
 
-
-  $scope.eventplan = function(datas) {
+  $scope.eventplan = function(data) {
+    console.log(data);
     var userId = "user-01";
-    var date = new Date();
-    var newPostKey = firebase.database().ref().child(date).push().key;
-    
+    var date = '150616';
+    /*firebase.database().ref(userId+'/').set({
+
+    })*/
+    var newPostKey = firebase.database().ref('user-01/').child(date).push().key;
+
     var updates = {};
-    updates['/' + userId + '/' + date + '/' + newPostKey] = datas.details;
-    updates['/' + userId + '/' + date + '/' + newPostKey + '/planning/start' + ] = datas.plan.start;
-    updates['/' + userId + '/' + date + '/' + newPostKey + '/planning/end' + ] = datas.plan.end;
+    updates['/' + userId + '/' + date + '/' + newPostKey + '/type'] = "localTravel";
+    updates['/' + userId + '/' + date + '/' + newPostKey + '/planning'] = data.plan;
+    //updates['/' + userId + '/' + date + '/' + newPostKey + '/planning/start'] = data.plan.start;
+    //updates['/' + userId + '/' + date + '/' + newPostKey + '/planning/end'] = data.plan.end;
 
     return firebase.database().ref().update(updates);
 
